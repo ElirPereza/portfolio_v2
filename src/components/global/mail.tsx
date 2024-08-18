dc3978531d5ff6bbf71b9341f3d5617ff94658a3
Jesus Maldonado <jmaldona18@cuc.edu.co>
2024-08-18 12:16:07 -0500

    Update mail.tsx

--- src/components/global/mail.tsx
+++ src/components/global/mail.tsx
@@ -1,9 +1,9 @@
-import { EmailTemplateProps } from "@/schema/contac-schema";
+import { contactSchemaType } from "@/schema/contac-schema";
 import * as React from "react";
 
 
 
-export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = (data) => (
+export const EmailTemplate: React.FC<Readonly<contactSchemaType>> = (data) => (
   <div>
     <h2>
       Client name: {data.firstName} {data.lastname}
