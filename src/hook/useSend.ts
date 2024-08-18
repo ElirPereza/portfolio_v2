e87a48c620414f656469c033027122afb04f07c0
Jesus Maldonado <jmaldona18@cuc.edu.co>
2024-08-18 09:55:56 -0500

    Update useSend.ts

--- src/hook/useSend.ts
+++ src/hook/useSend.ts
@@ -14,7 +14,7 @@ interface EmailTemplateProps {
 export const useSendEmail = () => {
   return useMutation({
     mutationFn: async (data: EmailTemplateProps) => {
-      const response = await axios.post('http://localhost:3000/api/send/', data);
+      const response = await axios.post('https://current-porfolio.vercel.app/api/send/', data);
       return response.data;
     },
   });
